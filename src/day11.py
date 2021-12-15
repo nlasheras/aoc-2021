13904b27663b61ae9ad3d10f00bbc13d6385cb6c
Nacho Lasheras <nacho@orcsandfireballs.com>
2021-12-15 15:50:59 +0100

    Add a utils library to re-use some code between days. Start with the Grid

--- src/day11.py
+++ src/day11.py
@@ -1,81 +1,41 @@
 # https://adventofcode.com/2021/day/11
 
-class Grid:
-    def __init__(self, filename):
-        with open(filename, "r", encoding='utf-8') as file:
-            lines = [l.rstrip() for l in file.readlines()]
-            self.rows = len(lines)
-            self.cols = len(lines[0])
-            self.cells = []
-            for l in lines:
-                self.cells += [int(c) for c in l]
+from utils import Grid
 
-    def get_idx(self, p):
-        if 0 <= p[0] < self.cols and 0 <= p[1] < self.rows:
-            return p[0] + p[1]*self.cols
+def octopus_step(grid):
+    flashed = [False] * grid.rows * grid.cols
 
-    def get_pos(self, idx):
-        row = idx // self.cols
-        col = idx % self.cols
-        return (col, row)
+    def flash_cell(idx):
+        neighbors = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+        flashed[idx] = True
+        pi = grid.get_pos(idx)
+        adjacent_indexes = list(filter(lambda x: x != None, 
+                                [grid.get_idx((pi[0] + delta[0], pi[1] + delta[1])) 
+                                    for delta in neighbors]))
+        for adj_i in adjacent_indexes:
+            if not flashed[adj_i]: 
+                grid.cells[adj_i] += 1 
 
-    def step(self):
-        flashed = [False] * self.rows * self.cols
-
-        def flash_cell(idx):
-            neighbors = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-            flashed[idx] = True
-            pi = self.get_pos(idx)
-            adjacent_indexes = list(filter(lambda x: x != None, 
-                                    [self.get_idx((pi[0] + delta[0], pi[1] + delta[1])) 
-                                        for delta in neighbors]))
-            for adj_i in adjacent_indexes:
-                if not flashed[adj_i]: 
-                    self.cells[adj_i] += 1 
-
-        def check_flash_iteration():
-            count = 0
-            for i in range(self.rows * self.cols):
-                if self.cells[i] > 9 and not flashed[i]:
-                    flash_cell(i)
-                    count += 1
-            return count
-
-        # add 1 to the octopus energy
-        self.cells = list(map(lambda x: x + 1, self.cells))
-
-        # keep checking for flash until no cell has flashed
+    def check_flash_iteration():
         count = 0
-        while (c := check_flash_iteration()) > 0:
-            count += c
-
-        # after all the flashing has happened we set the flashing octopus to 0
-        self.cells = [0 if v > 9 else v for v in self.cells]
-
+        for i in range(grid.rows * grid.cols):
+            if grid.cells[i] > 9 and not flashed[i]:
+                flash_cell(i)
+                count += 1
         return count
 
-    def render(self, highlight = []):
+    # add 1 to the octopus energy
+    grid.cells = list(map(lambda x: x + 1, grid.cells))
 
-        def get_str(i):
-            return str(self.cells[i])
+    # keep checking for flash until no cell has flashed
+    count = 0
+    while (c := check_flash_iteration()) > 0:
+        count += c
 
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
+    # after all the flashing has happened we set the flashing octopus to 0
+    grid.cells = [0 if v > 9 else v for v in grid.cells]
 
-        render = ""
-        for y in range(self.rows):
-            for x in range(self.cols):
-                idx = self.get_idx((x, y))
-                cell_str = get_str(idx)
-                if idx in highlight:
-                    cell_str = BOLD + cell_str + ENDC
-                render += cell_str
-            render += "\n" if y < self.rows - 1 else ""
-        return render
-
-    def print(self, hightlight = []):
-        print(self.render())
-
+    return count
 
 def part1_count_flashes(input):
     g = Grid(input)
@@ -83,7 +43,7 @@ def part1_count_flashes(input):
 
     sum = 0
     for i in range(1, steps + 1):
-        sum += g.step()
+        sum += octopus_step(g)
     
     print(f"How many total flashes are there after {steps} steps? {sum}")
 
@@ -96,7 +56,7 @@ def part2_find_synchronize_step(input):
     i = 0
     while flashes != total_octopuses:
         i += 1
-        flashes = g.step()
+        flashes = octopus_step(g)
     
     print(f"Octopuses synchronize after step {i}")
 
--- src/day15.py
+++ src/day15.py
@@ -1,56 +1,6 @@
 # https://adventofcode.com/2021/day/15
 
-class Grid:
-    def __init__(self, filename):
-        with open(filename, "r", encoding='utf-8') as file:
-            lines = [l.rstrip() for l in file.readlines()]
-            self.rows = len(lines)
-            self.cols = len(lines[0])
-            self.cells = []
-            for l in lines:
-                self.cells += [int(c) for c in l]
-
-    def get_idx(self, p):
-        if 0 <= p[0] < self.cols and 0 <= p[1] < self.rows:
-            return p[0] + p[1]*self.cols
-
-    def get_pos(self, idx):
-        row = idx // self.cols
-        col = idx % self.cols
-        return (col, row)
-
-    def neighbors(self, p):
-        valid = []
-        for delta in [(1,0), (0,1), (-1, 0), (0, -1)]:
-            pos = (p[0] + delta[0], p[1] + delta[1])
-            if (pos[0] >= 0 and pos[0] < self.cols and
-                pos[1] >= 0 and pos[1] < self.rows):
-                valid.append(pos)
-        return valid
-
-    def render(self, highlight = []):
-
-        def __get_str__(i):
-            return str(self.cells[i])
-
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        YELLOW = '\033[93m'
-
-        render = ""
-        for y in range(self.rows):
-            for x in range(self.cols):
-                idx = self.get_idx((x, y))
-                cell_str = __get_str__(idx)
-                if idx in highlight:
-                    cell_str = YELLOW + BOLD + cell_str + ENDC
-                render += cell_str
-            render += "\n" if y < self.rows - 1 else ""
-        return render
-
-    def print(self, highlight = []):
-        print(self.render(highlight))
-
+from utils import Grid
 from collections import defaultdict
 import math
 from functools import reduce
