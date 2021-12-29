67fcacb1b2a51a87e593fc973c06c1a4950a512b
Nacho Lasheras <nacho@orcsandfireballs.com>
2021-12-29 14:51:18 +0100

    Cleanup day 6

--- src/day6.py
+++ src/day6.py
@@ -1,4 +1,6 @@
-# https://adventofcode.com/2021/day/6
+""" https://adventofcode.com/2021/day/6 """
+
+import sys
 
 def simulate_1day_reference(fishes):
     new_fishes = [8 for n in fishes if n == 0] # new fishes spawn when counter is 0
@@ -6,10 +8,10 @@ def simulate_1day_reference(fishes):
     return fishes + new_fishes
 
 def simulate_1day_fast(counters):
-    """The fastest way to simulate the fishes is using a count of how many fishes 
+    """The fastest way to simulate the fishes is using a count of how many fishes
     have each timer value and just using those timers the fishes popping in front of
     the timer array get duplicated both as new fishes with time '8' and increasing
-    the value of the '6' counter 
+    the value of the '6' counter
     """
     reproducing_fishes, new_counters = counters[0], counters[1:] + [0]
     new_counters[6] += reproducing_fishes
@@ -17,19 +19,18 @@ def simulate_1day_fast(counters):
     return new_counters
 
 def simulate_fishes(fishes, days):
-    # I build the counters array with space for the 8 timer so I don't need 
+    # I build the counters array with space for the 8 timer so I don't need
     # to handle the edge case in the simulate function
     counters = [sum([1 for f in fishes if f == t]) for t in range(9)]
-    for d in range(days):
+    for _ in range(days):
         counters = simulate_1day_fast(counters)
     return sum(counters)
 
-import sys
 def main():
     input_file = sys.argv[1] if len(sys.argv) > 1 else "input6.txt"
     with open(input_file, newline='',encoding='utf-8') as file:
         fishes = [int(n) for n in file.readlines()[0].split(",")]
-        
+
         amount = simulate_fishes(fishes, 80)
         print(f"After 80 days = {amount} fishes")
 
@@ -43,7 +44,7 @@ def print_example():
 
     for i in range(18):
         fishes = simulate_1day_reference(fishes)
-        counters= simulate_1day_fast(counters)
+        counters = simulate_1day_fast(counters)
         print(f"After {i+1} day ({len(fishes)}, {sum(counters)}) {fishes}")
     print("\n")
 
