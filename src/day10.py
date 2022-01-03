2f76c305d1e9187a05b932ae26fb035c566c851b
Nacho Lasheras <nacho@orcsandfireballs.com>
2022-01-03 19:05:37 +0100

    Cleanup day 10

--- src/day10.py
+++ src/day10.py
@@ -1,4 +1,6 @@
-# https://adventofcode.com/2021/day/10
+""" https://adventofcode.com/2021/day/10 """
+
+from functools import reduce
 
 def read_input(filename):
     with open(filename, "r", encoding='utf-8') as file:
@@ -11,23 +13,22 @@ def parse(line):
     points = { ")": 3, "]": 57, "}": 1197, ">": 25137 }
     error_score = 0
     stack = []
-    for c in line:
-        if c in opening_map: # it's a closing character
-            expecting = opening_map[c]
+    for _c in line:
+        if _c in opening_map: # it's a closing character
+            expecting = opening_map[_c]
             top = stack[-1]
             if expecting == top:
                 stack.pop()
             else:
-                return points[c], stack
+                return points[_c], stack
         else:
-            stack.append(c)
+            stack.append(_c)
     return error_score, stack
 
 def part1_syntax_error(lines):
     tuples = [parse(l) for l in lines]
     print(f"What is the total syntax error score for those errors? {sum([t[0] for t in tuples])}")
 
-from functools import reduce
 def part2_autocompletion(lines):
     valid_lines = filter(lambda t: t[0] == 0, [parse(l) for l in lines]) # valid lines have score 0
 
@@ -40,13 +41,11 @@ def part2_autocompletion(lines):
     middle_score = sorted(scores)[len(scores)//2]
     print(f"What is the middle score? {middle_score}")
 
+if __name__ == '__main__':
+    input_lines = read_input("input10_test.txt")
+    part1_syntax_error(input_lines)
+    part2_autocompletion(input_lines)
 
-input = read_input("input10_test.txt")
-part1_syntax_error(input)
-part2_autocompletion(input)
-
-input = read_input("input10.txt")
-part1_syntax_error(input)
-part2_autocompletion(input)
-
-
+    input_lines = read_input("input10.txt")
+    part1_syntax_error(input_lines)
+    part2_autocompletion(input_lines)
