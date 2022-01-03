dcdc50bf70bb57bc2f463ab9d7d31ec58a5d3185
Nacho Lasheras <nacho@orcsandfireballs.com>
2022-01-03 19:11:36 +0100

    Cleanup day 11

--- src/day11.py
+++ src/day11.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2021/day/11
+""" https://adventofcode.com/2021/day/11 """
 
 from utils import Grid
 
@@ -8,13 +8,13 @@ def octopus_step(grid):
     def flash_cell(idx):
         neighbors = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
         flashed[idx] = True
-        pi = grid.get_pos(idx)
-        adjacent_indexes = list(filter(lambda x: x != None, 
-                                [grid.get_idx((pi[0] + delta[0], pi[1] + delta[1])) 
+        p_i = grid.get_pos(idx)
+        adjacent_indexes = list(filter(lambda x: not x is None,
+                                [grid.get_idx((p_i[0] + delta[0], p_i[1] + delta[1]))
                                     for delta in neighbors]))
         for adj_i in adjacent_indexes:
-            if not flashed[adj_i]: 
-                grid.cells[adj_i] += 1 
+            if not flashed[adj_i]:
+                grid.cells[adj_i] += 1
 
     def check_flash_iteration():
         count = 0
@@ -29,35 +29,35 @@ def octopus_step(grid):
 
     # keep checking for flash until no cell has flashed
     count = 0
-    while (c := check_flash_iteration()) > 0:
-        count += c
+    while (flashed_it := check_flash_iteration()) > 0:
+        count += flashed_it
 
     # after all the flashing has happened we set the flashing octopus to 0
     grid.cells = [0 if v > 9 else v for v in grid.cells]
 
     return count
 
-def part1_count_flashes(input):
-    g = Grid(input)
+def part1_count_flashes(input_file):
+    grid = Grid.from_file(input_file)
     steps = 100
 
-    sum = 0
-    for i in range(1, steps + 1):
-        sum += octopus_step(g)
-    
-    print(f"How many total flashes are there after {steps} steps? {sum}")
+    total_flashes = 0
+    for _ in range(1, steps + 1):
+        total_flashes += octopus_step(grid)
 
-def part2_find_synchronize_step(input):
-    g = Grid(input)
+    print(f"How many total flashes are there after {steps} steps? {total_flashes}")
 
-    total_octopuses = g.cols * g.rows
+def part2_find_synchronize_step(input_file):
+    grid = Grid.from_file(input_file)
+
+    total_octopuses = grid.cols * grid.rows
 
     flashes = 0
     i = 0
     while flashes != total_octopuses:
         i += 1
-        flashes = octopus_step(g)
-    
+        flashes = octopus_step(grid)
+
     print(f"Octopuses synchronize after step {i}")
 
 if __name__ == '__main__':
