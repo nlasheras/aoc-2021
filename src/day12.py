65be971fc3ee44ed9f0630025a25caae108784f7
Nacho Lasheras <nacho@orcsandfireballs.com>
2022-01-03 19:38:44 +0100

    Cleanup day 12

--- src/day12.py
+++ src/day12.py
@@ -1,11 +1,13 @@
-# https://adventofcode.com/2021/day/12
+""" https://adventofcode.com/2021/day/12 """
 
 from collections import defaultdict
+import re
 
 class Graph:
+    """A graph implemented with a dictionary of adjacency between nodes"""
     def __init__(self):
         self.adjacency = defaultdict(lambda: [])
-    
+
     def add_edge(self, start, end):
         self.adjacency[start] += [end]
         self.adjacency[end] += [start]
@@ -13,68 +15,69 @@ class Graph:
     def neighbors(self, node):
         return sorted(self.adjacency[node])
 
-import re
-def read_input(filename):
-    with open(filename, "r") as file:
-        g = Graph()
-        lines = file.readlines()
-        r = re.compile("(\w+)-(\w+)")
-        for l in lines:
-            if match := r.search(l):
-                g.add_edge(match.group(1), match.group(2))
-        return g
+    @staticmethod
+    def from_file(filename):
+        with open(filename, "r", encoding="utf-8") as file:
+            ret = Graph()
+            edge_re = re.compile(r"(\w+)-(\w+)")
+            for _l in file.readlines:
+                if match := edge_re.search(_l):
+                    ret.add_edge(match.group(1), match.group(2))
+            return ret
 
-def find_paths(g, nodes, can_be_in_path):
+def find_paths(graph, nodes, can_be_in_path):
+    """This function implements a recursive DF traversal on the graph checking the neighbors
+    with a function so we can re-use it for both parts"""
     paths = []
-    for n in g.neighbors(nodes[-1]):
-        if n == 'end':
-            paths += [nodes + [n]]
-        elif not can_be_in_path(nodes, n):
-            continue 
+    for node in graph.neighbors(nodes[-1]):
+        if node == 'end':
+            paths += [nodes + [node]]
+        elif not can_be_in_path(nodes, node):
+            continue
         else:
-            paths += find_paths(g, nodes + [n], can_be_in_path)
+            paths += find_paths(graph, nodes + [node], can_be_in_path)
     return paths
-    
+
 
 def part1_count_paths(filename):
-    g = read_input(filename)
-    
-    def part1_cond(path, n):
-        if n.islower() and n in path:
+    caves = Graph.from_file(filename)
+
+    def part1_cond(path, node):
+        if node.islower() and node in path:
             return False
         return True
 
-    paths = find_paths(g, ["start"], part1_cond)
+    paths = find_paths(caves, ["start"], part1_cond)
 
     print(f"How many paths through this cave system are there that visit small caves at most once? {len(paths)}")
 
 def part2_visit_small_caves_twice(filename):
-    g = read_input(filename)
-    
-    def part2_cond(path, n):
-        if n.isupper():
+    caves = Graph.from_file(filename)
+
+    def part2_cond(path, node):
+        if node.isupper():
             return True
-        elif n == "start" and n in path:
+        if node == "start" and node in path:
             return False # start can only be visited once
-        elif not n in path:
+        if not node in path:
             return True
 
         counts = {n: sum([1 for e in path if e == n]) for n in path if n.islower()}
-        counts[n] += 1 # assume it's in path
+        counts[node] += 1 # assume it's in path
 
-        if counts[n] > 2: # a single node can be visited at most twice
+        if counts[node] > 2: # a single node can be visited at most twice
             return False
 
-        s = sum([1 for e in counts.values() if e >= 2])
-        return  s <= 1
+        caves_visited_twice = sum([1 for e in counts.values() if e >= 2])
+        return  caves_visited_twice <= 1
 
-    paths = find_paths(g, ["start"], part2_cond)
+    paths = find_paths(caves, ["start"], part2_cond)
     print(f"How many paths through this cave system are there that visit small caves at most once? {len(paths)}")
 
 if __name__ == '__main__':
     part1_count_paths("input12_test2.txt")
     part1_count_paths("input12.txt")
-    
+
     part2_visit_small_caves_twice("input12_test1.txt")
     part2_visit_small_caves_twice("input12_test2.txt")
     part2_visit_small_caves_twice("input12_test3.txt")
