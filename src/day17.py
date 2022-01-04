0ba6dad7772de11e8571f1e8173877376d972815
Nacho Lasheras <nacho@orcsandfireballs.com>
2022-01-04 19:16:26 +0100

    Cleanup day 17

--- src/day17.py
+++ src/day17.py
@@ -1,66 +1,80 @@
-# https://adventofcode.com/2021/day/17
+""" https://adventofcode.com/2021/day/17 """
 
 import re
+import math
+from typing import Tuple
+
 class Rect:
-    def __init__(self, left, right, bottom, up):
+    """A 2D rectangle defined by top-left and bottom-right positions"""
+    def __init__(self, left, right, bottom, top):
         self.left = left
         self.right = right
         self.bottom = bottom
-        self.up = up
-    
+        self.top = top
+
     def inside(self, x, y):
-        if (self.left <= x <= self.right) and (self.bottom <= y <= self.up):
-           return True
-        return False
+        """Checks if a given x, y point is inside the rect"""
+        return (self.left <= x <= self.right) and (self.bottom <= y <= self.top)
 
+    @staticmethod
     def from_input(string):
-        match = re.search("target area: x=(-?\d*)..(-?\d*), y=(-?\d*)..(-?\d*)", string)
+        match = re.search(r"target area: x=(-?\d*)..(-?\d*), y=(-?\d*)..(-?\d*)", string)
         if match:
-            return Rect(int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)))
+            left = int(match.group(1))
+            right = int(match.group(2))
+            bottom = int(match.group(3))
+            top = int(match.group(4))
+            return Rect(left, right, bottom, top)
+        assert False # Shouldn't reach
+        return None
 
-def sign(n):
-    if n > 0: return 1
-    elif n < 0: return -1
+def sign(_n):
+    if _n > 0:
+        return 1
+    if _n < 0:
+        return -1
     return 0
 
-def hit_target(vx0, vy0, target):
-    vx = vx0
-    vy = vy0
-    px = 0
-    py = 0
-    t = 0
+def hit_target(vx0, vy0, target:Rect) -> Tuple[bool, int]:
+    """Simulate the probe shooting and check if the probe reaches the target area.
+    Returns wether probe reaches the target area in a discrete t and, in that case,
+    the maximum height it reaches on the trajectory."""
+    velocity_x = vx0
+    velocity_y = vy0
+    probe_x = 0
+    probe_y = 0
+    _t = 0
     max_height = 0
-    while px < target.right and py > target.bottom: 
-        px += vx
-        py += vy
-        max_height = max(max_height, py)
-        vx -= sign(vx)
-        vy -= 1
-        t += 1
-        if target.inside(px, py):
+    while probe_x < target.right and probe_y > target.bottom:
+        probe_x += velocity_x
+        probe_y += velocity_y
+        max_height = max(max_height, probe_y)
+        velocity_x -= sign(velocity_x)
+        velocity_y -= 1
+        _t += 1
+        if target.inside(probe_x, probe_y):
             return True, max_height
     return False, 0
 
 puzzle = Rect.from_input("target area: x=209..238, y=-86..-59")
 example = Rect.from_input("target area: x=20..30, y=-10..-5")
 
-import math
 def both_parts_bruteforce(target):
     global_maxima = 0
     hit_count = 0
-    
+
     # do a smart brute-force over sensible ranges
-    minvx = 0
-    maxvx = target.right # max speed is hitting the right of the area in t=1
+    min_vx = 0
+    max_vx = target.right # max speed is hitting the right of the area in t=1
 
-    minvy = min(target.bottom, target.up) # use the same reasoning as for maxvy
-    maxvy = -minvy # not much thinkin here (explore the same range in the positive than the negatives)
+    min_vy = min(target.bottom, target.top) # use the same reasoning as for maxvy
+    max_vy = -min_vy # not much thinkin here (explore the same range in positive than in negative)
 
-    for vx in range(minvx, maxvx+1):
-        minvx = math.floor((1 + math.sqrt(1 + target.left * 8)) / 2)
-        maxvx = target.right
-        for vy in range(minvy, maxvy+1):
-            hit, maxy = hit_target(vx, vy, target)
+    for velocity_x in range(min_vx, max_vx+1):
+        min_vx = math.floor((1 + math.sqrt(1 + target.left * 8)) / 2)
+        max_vx = target.right
+        for velocity_y in range(min_vy, max_vy+1):
+            hit, maxy = hit_target(velocity_x, velocity_y, target)
             if hit:
                 global_maxima = max(global_maxima, maxy)
                 hit_count += 1
